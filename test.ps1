f80d82bbd0d5a4bb587d0d185573b5ea1debe535
Niels Ballis <n.ballis@hotmail.com>
2020-01-13 23:52:21 +0100

    first tests

--- test.ps1
+++ test.ps1
@@ -1,2 +1,21 @@
-$apiurl = "https://eu.api.blizzard.com/wow/auction/data/blades-edge?access_token=USu0wJCblCmX5WtHhvBNlSmsrhOXf0
-$apiaccess
+## https://eu.api.blizzard.com/data/wow/item/4305?namespace=static-eu&locale=en_GB&access_token=USu0wJCblCmX5WtHhvBNlSmsrhOXf0dEW9
+
+if (!($ahauctions)) {
+    $apiurl = "https://eu.api.blizzard.com/wow/auction/data/blades-edge?access_token=USu0wJCblCmX5WtHhvBNlSmsrhOXf0dEW9"
+    $apirequest = Invoke-WebRequest $apiurl
+    $ahurl = ($apirequest | ConvertFrom-Json).files.url
+    $ahauctions = Invoke-WebRequest $ahurl
+    $ahauctions = ($ahauctions.Content | convertfrom-json).auctions
+    
+}
+
+foreach ($auction in $ahauctions) {
+    $apirequest = "https://eu.api.blizzard.com/data/wow/item/$($auction.item)?namespace=static-eu&locale=en_GB&access_token=USu0wJCblCmX5WtHhvBNlSmsrhOXf0dEW9"
+    $iteminfo = Invoke-WebRequest $apirequest
+    $iteminfo = $iteminfo.Content | ConvertFrom-Json
+    $auction | Add-Member -MemberType NoteProperty -Name "itemname" -Value $iteminfo.name
+    $auction | Add-Member -MemberType NoteProperty -Name "itemclass" -Value $iteminfo.item_class.id
+    $auction | Add-Member -MemberType NoteProperty -Name "vendorprice" -Value $iteminfo.sell_price
+}
+
+$ahauctions | convertto-csv | out-file ".\auctions.csv"
